55b79360b8773b11cd093c5afe9abd181148406d
carlosmaeda3d <carlosmaeda3d@yahoo.com>
2022-07-28 22:35:27 -0400

    commit 4

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ class MonthData:
                + self.wk_expenses + "," + self.wk_total
 
 class WeekData:
-    def __init__(self, wk_month, wk_num, wk_item_num, wk_item_type, wk_item_name, wk_item_amount):
+    def __init__(self, wk_month, wk_num, wk_item_type, wk_item_name, wk_item_amount):
         self.wk_month = wk_month
         self.wk_num = wk_num
-        self.wk_item_num = wk_item_num
         self.wk_item_type = wk_item_type
         self.wk_item_name = wk_item_name
         self.wk_item_amount = wk_item_amount
+
+    def __repr__(self):
+        return self.wk_month + ", " + str(self.wk_num) + ", " + self.wk_item_type + ", " + self.wk_item_name + ", " + \
+               str(self.wk_item_amount) + "\n"
 
 class BudgetApp:
 
@@ -39,19 +42,15 @@ class BudgetApp:
                   "October", "November", "December")
         month_text = StringVar()
         current_month = IntVar()
-        month_list = [
-            # MonthData("July", "Week 1", "Test Range", 4000, 2000, 2000),
-            # MonthData("July", "Week 2", "Test Range", 2000, 1000, 1000),
-            # MonthData("August", "Week 1", "Test Range", 2000, 1000, 1000)
-        ]
+        month_list = []
         week_list = [
-            WeekData("July", 1, 1, "Income", "Paycheck", 2000),
-            WeekData("July", 1, 2, "Income", "Paycheck2", 2000),
-            WeekData("July", 1, 3, "Expense", "Fun Time", 2000),
-            WeekData("July", 2, 1, "Income", "Paycheck", 3000),
-            WeekData("July", 2, 2, "Expense", "Fun Time", 1000),
-            WeekData("August", 1, 1, "Income", "Paycheck", 2000),
-            WeekData("August", 1, 2, "Expense", "Fun Time", 1000)
+            WeekData("July", 1, "Income", "Paycheck", 5000),
+            WeekData("July", 1, "Income", "Paycheck2", 4000),
+            WeekData("July", 1, "Expense", "Fun Time", 8000),
+            WeekData("July", 2, "Income", "Paycheck", 6000),
+            WeekData("July", 2, "Expense", "Fun Time", 1000),
+            WeekData("August", 1, "Income", "Paycheck", 2000),
+            WeekData("August", 1, "Expense", "Fun Time", 1000)
         ]
         main_selected_week = StringVar()
         # Setting start month
@@ -75,7 +74,6 @@ class BudgetApp:
             for i in range(weeks):
                 total_income = 0
                 total_expense = 0
-                grant_total = total_income - total_expense
 
                 for m in week_list:
                     if m.wk_month == month_text.get():
@@ -85,16 +83,22 @@ class BudgetApp:
                             elif m.wk_item_type == "Expense":
                                 total_expense += m.wk_item_amount
 
+                grant_total = total_income - total_expense
+
                 new_item = MonthData(month_text.get(), "Week " + str(i + 1), "Range", str(total_income),
                                      str(total_expense), str(grant_total))
                 month_list.append(new_item)
-            print(month_list)
+            # print(month_list)
 
         def month_frame():
             frame1 = Frame(root, width=800, height=300, bg=bg_green)
             frame1.grid(row=0, column=0)
             frame1.pack_propagate(False)
             frame1.grid_propagate(False)
+
+            # Clears month list then creates new one so it doesn't keep
+            month_list.clear()
+            month_list_creator()
 
             def month_tree_delete():
                 # Deletes items in tree
@@ -114,6 +118,10 @@ class BudgetApp:
 
                 # Changes month text to new month
                 month_text.set(months[current_month.get()])
+
+                # Clears month list then creates new one so it doesn't keep
+                month_list.clear()
+                month_list_creator()
 
                 # Deletes items in tree and adds new month
                 month_tree_delete()
@@ -136,6 +144,10 @@ class BudgetApp:
                 # Changes month text to new month
                 month_text.set(months[current_month.get()])
 
+                # Clears month list then creates new one so it doesn't keep
+                month_list.clear()
+                month_list_creator()
+
                 # Deletes items in tree and adds new month
                 month_tree_delete()
                 for i in month_list:
@@ -149,7 +161,6 @@ class BudgetApp:
 
             def delete_week():
                 print("Delete Last Week")
-                month_list_creator()
                 # month_length = len(self.month_tree.co)
                 # print(month_length)
 
@@ -220,8 +231,13 @@ class BudgetApp:
             frame2.pack_propagate(False)
             frame2.grid_propagate(False)
 
-            #///TESTING ITEMS- DELETE WHEN DONE TESTING!!!!!!!!!!!
-            current_week = 1
+            def week_trees_delete():
+                # Deletes items in tree
+                for item in self.income_tree.get_children():
+                    self.income_tree.delete(item)
+
+                for item in self.expense_tree.get_children():
+                    self.expense_tree.delete(item)
 
             def income_clicker(event):
                 # Clears entries
@@ -234,11 +250,14 @@ class BudgetApp:
                 # Grab the record number
                 selected = self.income_tree.focus()
                 # Grab record values
-                values = self.income_tree.item(selected, 'values')
-                # Output to entry boxes
-                name_input.set(values[0])
-                amount_input.set(values[1])
-                type_select.set("Income")
+                values = self.income_tree.item(selected, "values")
+                # Output to entry boxes. It first checks if is selected, else deselect to prevent index range error
+                if len(self.income_tree.selection()) > 0:
+                    name_input.set(values[0])
+                    amount_input.set(values[1])
+                    type_select.set("Income")
+                else:
+                    clear_item()
 
             def expense_clicker(event):
                 # Clears entries and selections
@@ -249,15 +268,46 @@ class BudgetApp:
 
                 # Grab the record number
                 selected = self.expense_tree.focus()
+
                 # Grab record values
-                values = self.expense_tree.item(selected, 'values')
-                # Output to entry boxes
-                name_input.set(values[0])
-                amount_input.set(values[1])
-                type_select.set("Expense")
+                values = self.expense_tree.item(selected, "values")
+
+                # Output to entry boxes. It first checks if is selected, else deselect to prevent index range error
+                if len(self.expense_tree.selection()) > 0:
+                    name_input.set(values[0])
+                    amount_input.set(values[1])
+                    type_select.set("Expense")
+                else:
+                    clear_item()
 
             def delete_item():
-                print("Delete")
+                # Figures out what is selected and deletes. Gives error message is nothing is selected
+                if len(self.income_tree.selection()) > 0 or len(self.expense_tree.selection()) > 0:
+                    if len(self.income_tree.selection()) > 0:
+                        selected = self.income_tree.selection()[0]
+                        values = self.income_tree.item(selected, "values")
+                        for i in week_list:
+                            if i.wk_month == month_text.get() and i.wk_num == int(main_selected_week.get()[5]) \
+                                    and i.wk_item_type == "Income":
+                                if i.wk_item_name == values[0] and i.wk_item_amount == int(values[1]):
+                                    week_list.remove(i)
+                        self.income_tree.delete(selected)
+
+                    elif len(self.expense_tree.selection()) > 0:
+                        selected = self.expense_tree.selection()[0]
+                        values = self.expense_tree.item(selected, "values")
+                        for i in week_list:
+                            if i.wk_month == month_text.get() and i.wk_num == int(main_selected_week.get()[5]) \
+                                    and i.wk_item_type == "Expense":
+                                if i.wk_item_name == values[0] and i.wk_item_amount == int(values[1]):
+                                    week_list.remove(i)
+                        self.expense_tree.delete(selected)
+                else:
+                    messagebox.showinfo("Error", "No item selected")
+                    return
+
+                # Clear inputs
+                clear_item()
 
             def clear_item():
                 name_input.set("")
@@ -267,10 +317,56 @@ class BudgetApp:
                 self.expense_tree.selection_toggle(self.expense_tree.selection())
 
             def save_item():
-                print("Save")
+                # Gives error if anything other than int is used in item amount
+                try:
+                    val = int(amount_input.get())
+                except:
+                    messagebox.showinfo("Error", "AMOUNT: Need to input a number")
+                    return
+
+                #\\\\\\\\\\\THINK OF A BETTER WAY FOR MAKING SURE NOT BLANK!! CAN BYPASS WITH SPACE!!!///////////////
+                # Gives error if name is blank
+                if name_input.get() in ("", " ", "  "):
+                    messagebox.showinfo("Error", "NAME: Can't leave this item blank")
+                    return
+
+                if type_select.get() == "":
+                    messagebox.showinfo("Error", "Type: Can't leave this item blank")
+                    return
+
+                # Gives error if name matches another name in week. If not it will cause issue with deleting
+                for i in week_list:
+                    if i.wk_month == month_text.get() and i.wk_num == int(main_selected_week.get()[5]) \
+                            and i.wk_item_type == type_select.get() and i.wk_item_name == name_input.get():
+                        messagebox.showinfo("Error", "NAME: Please rename. Can't match name from week.")
+                        return
+
+                # Adding item to week list
+                new_item = WeekData(month_text.get(), int(main_selected_week.get()[5]), type_select.get(),
+                                    name_input.get(), int(amount_input.get()))
+                week_list.append(new_item)
+
+                # Deletes trees and resets them
+                week_trees_delete()
+                for i in week_list:
+                    if i.wk_month == wk_frame_month.get():
+                        if i.wk_num == wk_frame_week.get():
+                            if i.wk_item_type == "Income":
+                                self.income_tree.insert("", "end", values=(i.wk_item_name, i.wk_item_amount))
+                            elif i.wk_item_type == "Expense":
+                                self.expense_tree.insert("", "end", values=(i.wk_item_name, i.wk_item_amount))
+
+                # Clear inputs
+                clear_item()
+                # print(week_list)
 
             def back_month():
                 frame_delete(frame2)
+
+                # Clears month list then creates new one so it doesn't keep
+                month_list.clear()
+                month_list_creator()
+
                 month_frame()
 
             #Labels
@@ -292,12 +388,10 @@ class BudgetApp:
             expenses_label.place(x=300, y=50)
             edit_label.place(x=600, y=50)
 
-            #Week number string
-            # if main_selected_week == 1 or 2 or 3 or 4 or 5:
-            #     week_label_text.set(main_selected_week)
+            # Week number string
             week_label_text.set(main_selected_week.get())
 
-            #Labels-Edit Item Area
+            # Labels-Edit Item Area
             edit_labels_xstart = 640
             edit_labels_ystart = 95
             type_label = tk.Label(frame2, text="Type:", font=("TkHeadingFont", 11, "bold", "underline"),
@@ -312,7 +406,7 @@ class BudgetApp:
             amount_label.place(x=edit_labels_xstart, y=edit_labels_ystart + 70, anchor=E)
 
 
-            #Buttons
+            # Buttons
             back_button = tk.Button(frame2, text="Month Sheet", font=("TkHeadingFont", 11), bg=dark_green,
                                           fg="white", width=13, cursor="hand2", command=back_month)
             save_button = tk.Button(frame2, text="Save", font=("TkHeadingFont", 11), bg=dark_green,
@@ -326,7 +420,7 @@ class BudgetApp:
             clear_button.place(x=680, y=200)
             delete_button.place(x=600, y=250)
 
-            #Combobox & Entries
+            # Combobox & Entries
             type_select = StringVar()
             name_input = StringVar()
             amount_input = StringVar()
@@ -334,19 +428,19 @@ class BudgetApp:
             name_entry = tk.Entry(frame2, width=20, textvariable=name_input)
             amount_entry = tk.Entry(frame2, width=20, textvariable=amount_input)
 
-            type_chooser = ttk.Combobox(frame2, width=10, textvariable=type_select)
+            type_chooser = ttk.Combobox(frame2, width=10, state="readonly", textvariable=type_select)
             type_chooser["values"] = ["Income", "Expense"]
 
             type_chooser.place(x=edit_labels_xstart + 10, y=edit_labels_ystart, anchor=W)
             name_entry.place(x=edit_labels_xstart + 10, y=edit_labels_ystart + 35, anchor=W)
             amount_entry.place(x=edit_labels_xstart + 10, y=edit_labels_ystart + 70, anchor=W)
 
-            #Treeviews
+            # Treeviews
             # TREEVIEW::::: Define Columns
             wk_frame_month = StringVar()
             wk_frame_month.set(month_text.get())
             wk_frame_week = IntVar()
-            wk_frame_week.set(current_week)
+            wk_frame_week.set(int(main_selected_week.get()[5]))
 
             income_tree_columns = ("income_item_name", "income_item_amount")
             expense_tree_columns = ("expense_item_name", "expense_item_amount")
@@ -380,7 +474,7 @@ class BudgetApp:
                             self.expense_tree.insert("", "end", values=(i.wk_item_name, i.wk_item_amount))
 
             # Binding trees to text boxes with single click using button release. NO SINGLE CLICK ACTION
-            # Double click binf is "<Double-1>"
+            # Double click bind is "<Double-1>"
             self.income_tree.bind("<ButtonRelease-1>", income_clicker)
             self.expense_tree.bind("<ButtonRelease-1>", expense_clicker)
 
@@ -391,14 +485,57 @@ class BudgetApp:
 
 
         def edit_month_frame():
-            frame3 = Frame(root, width=400, height=200, bg=bg_green)
+            frame3 = Frame(root, width=300, height=175, bg=bg_green)
             frame3.grid(row=0, column=0)
             frame3.pack_propagate(False)
             frame3.grid_propagate(False)
 
-        month_frame()
+            def save_month():
+                print("Save")
+
+            def back_month():
+                frame_delete(frame3)
+
+                # Clears month list then creates new one so it doesn't keep
+                month_list.clear()
+                month_list_creator()
+
+                month_frame()
+
+            # Header
+            frame_label = tk.Label(frame3, text="Edit Month", font=("TkHeadingFont", 20, "bold", "underline"),
+                                      fg=dark_green, bg=bg_green)
+            frame_label.pack()
+
+            # Start Month Date
+            date_input = StringVar()
+
+            start_label = tk.Label(frame3, text="Start Date", font=("TkHeadingFont", 13, "bold", "underline"),
+                                      fg=dark_green, bg=bg_green)
+            start_input = tk.Entry(frame3, width=10, textvariable=date_input)
+            start_label.place(x=20, y=50)
+            start_input.place(x=30, y=80)
+
+            # How many weeks drop down
+            many_weeks_label = tk.Label(frame3, text="How many weeks?", font=("TkHeadingFont", 13, "bold", "underline"),
+                                fg=dark_green, bg=bg_green)
+            week_sel_combo = ttk.Combobox(frame3, width=3, state="readonly")
+            week_sel_combo["values"] = [1, 2, 3, 4, 5]
+            many_weeks_label.place(x=130, y=50)
+            week_sel_combo.place(x=180, y=80)
+
+            # Buttons
+            back_button = tk.Button(frame3, text="Month Sheet", font=("TkHeadingFont", 11), bg=dark_green,
+                                    fg="white", width=13, cursor="hand2", command=back_month)
+
+            save_button = tk.Button(frame3, text="Save", font=("TkHeadingFont", 11), bg=dark_green,
+                                    fg="white", width=13, cursor="hand2", command=save_month)
+            back_button.place(x=20, y=105)
+            save_button.place(x=150, y=105)
+
+        # month_frame()
         # week_frame()
-        # edit_month_frame())
+        edit_month_frame()
 
 # Initualize app
 root = Tk()
